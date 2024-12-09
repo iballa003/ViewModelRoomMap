b1de2827d44c98492a6111c1f04ffa963df76fd8
iballa003 <iballa.montero@gmail.com>
2024-12-09 17:16:52 +0000

    Rellenado el archivo Database.kt

--- app/src/main/java/org/iesharia/viewmodelroommap/data/Database.kt
+++ app/src/main/java/org/iesharia/viewmodelroommap/data/Database.kt
@@ -1,2 +1,27 @@
 package org.iesharia.viewmodelroommap.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Mark::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun markDao(): MarkDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "map_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
